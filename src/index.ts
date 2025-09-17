a9286f774b56f5fd24c95a0bdda6a7576d0bb304
Fernando Moreti <fernandombolela@gmail.com>
2025-09-17 10:13:46 -0300

    Adicionando um txt para explicação e aprimorando o nosso mini chat

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,38 @@
 import dotenv from "dotenv"
+import OpenAI from "openai"
 dotenv.config();
 
-console.log("API key:" , process.env.OPEN_AI_SECRET_API_KEY)
+const client = new OpenAI({
+    apiKey: process.env.OPEN_AI_SECRET_API_KEY
+})
+
+async function generateText() {
+
+    const completion = await client.chat.completions.create({
+        model: "gpt-4o-mini",
+        max_completion_tokens: 100,
+        messages: [
+            {
+                role: "developer",
+                content: "User emogis a cada 2 palavras"
+            },
+            {
+                role: "user",
+                content: "Escreva uma mensagem de uma frase sobre unicornios"
+            },
+            {
+                role: "assistant",
+                content: "//mensagem digitada pelo chat"
+            },
+            {
+                role: "user",
+                content: "Obrigado"
+            },
+        ]
+    })
+
+    console.log(completion.choices[0].message.content)
+
+}
+
+generateText()
