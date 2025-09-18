7d15482d8446621c8de696a75a773929e17e9161
Fernando Moreti <fernandombolela@gmail.com>
2025-09-17 21:22:55 -0300

    Structured Outputs e entendendo um pouco de Zod

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,9 @@
 import dotenv from "dotenv"
-import OpenAI from "openai"
+import app from "./app";
 dotenv.config();
 
-const client = new OpenAI({
-    apiKey: process.env.OPEN_AI_SECRET_API_KEY
+const PORT = process.env.PORT || 8000
+
+app.listen(PORT, () => {
+    console.log("server on")
 })
-
-async function generateText() {
-
-    const completion = await client.chat.completions.create({
-        model: "gpt-4o-mini",
-        max_completion_tokens: 100,
-        messages: [
-            {
-                role: "developer",
-                content: "User emogis a cada 2 palavras"
-            },
-            {
-                role: "user",
-                content: "Escreva uma mensagem de uma frase sobre unicornios"
-            },
-            {
-                role: "assistant",
-                content: "//mensagem digitada pelo chat"
-            },
-            {
-                role: "user",
-                content: "Obrigado"
-            },
-        ]
-    })
-
-    console.log(completion.choices[0].message.content)
-
-}
-
-generateText()
